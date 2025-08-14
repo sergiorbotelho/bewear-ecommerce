34420b29e19e1b7e286731cacbe32af5ea103201
Sergio Botelho <sergiorbotelho@gmail.com>
2025-08-13 22:37:58 -0300

    feat: ensure shipping address can be created

---
+++ src/hooks/mutations/use-create-shipping-address.ts
@@ -0,0 +1,13 @@
+import { useMutation } from "@tanstack/react-query";
+
+import { createShippingAddress } from "@/actions/create-shippping-address";
+
+export const getCreateShippingAddressMutationKey = () =>
+  ["create-shipping-address"] as const;
+
+export const useCreateShippingAddress = () => {
+  return useMutation({
+    mutationKey: getCreateShippingAddressMutationKey(),
+    mutationFn: createShippingAddress,
+  });
+};
